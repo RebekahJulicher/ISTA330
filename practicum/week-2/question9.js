9801b20012cad5d6de91b20853fdd5d7f51238b3
RebekahJulicher <70235428+RebekahJulicher@users.noreply.github.com>
2020-09-17 13:05:13 -0700

    The Long Way #9

--- practicum/week-2/question9.js
+++ practicum/week-2/question9.js
@@ -13,8 +13,57 @@ so there are two groups with the largest size.
 */
 
 var largestGroupsCount = function(n) {
-    if (n % 9 != 0){
-        return n % 9
+    if (n < 19){
+        if (n % 9 != 0){
+            return (n % 9)
+        }
+        return (9)
     }
-    return 9
+    let trackerList = []
+
+    for (let i = 1; i <= n; i++){
+        let sum = 0;
+        while (value) {
+            sum += value % 10;
+            value = Math.floor(n / 10);
+        }
+        let found = false;
+        for (let numList of trackerList){
+            if (numList[0] == sum){
+                numList.push(sum);
+                found = true;
+            }
+        }
+        if (found == false){
+            trackerList.push([sum])
+        }
+    }
+
+    let longest = 0;
+    for (let item of trackerList){
+        if (item.length > longest){
+            longest = item.length;
+        }
+    }
+    let output = 0;
+    for (let item of trackerList){
+        if (item.length == longest){
+            output += 1;
+        }
+    }
+
+    return output;
+
+    /*
+    i = 0
+    while ((19 + (9 * (i))) <= n){
+        i += 1
+    }
+    if (i < 9){
+        if (n % 9 != 0){
+            return ((n % 9) - i)
+        }
+        return (9 - i)
+    }
+    */
 };
