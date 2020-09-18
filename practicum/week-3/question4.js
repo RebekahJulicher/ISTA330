217584d4453ab179220e91e508cf9746d66b0481
RebekahJulicher <70235428+RebekahJulicher@users.noreply.github.com>
2020-09-17 21:44:05 -0700

    Q4

--- practicum/week-3/question4.js
+++ practicum/week-3/question4.js
@@ -5,7 +5,7 @@ return all possible subsets (the power set).
 The solution set must not contain duplicate subsets.
  
 Example:
- input: [1,2,3,4,5]
+ input: [1,2,3]
  output: [
   [],   
   [1],
@@ -21,5 +21,20 @@ Example:
 var powerSet = function(input) {
   //running total list and for lists, return running total list
   // and the number and check number mini list
+  let output = [];
+  output.push([]);
+  output.push(input);
+  for (let i = 0; i < input.length; i++){
+    let current = input[i];
+    let runningTotal = [current];
+    for (let j = i + 1; j < input.length; j++){
+      runningTotal.push(input[j]);
+      output.push([current, input[j]]);
+      if (output.includes(runningTotal) == false){
+        output.push(runningTotal);
+      }
+    }
+  }
+  return output;
 
 };
