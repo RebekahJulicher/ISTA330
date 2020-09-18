d228d93adade0bba1266ae6520f9034a393f3398
RebekahJulicher <70235428+RebekahJulicher@users.noreply.github.com>
2020-09-17 22:58:41 -0700

    Question 9

--- practicum/week-3/question9.js
+++ practicum/week-3/question9.js
@@ -11,5 +11,27 @@ output: 1
 */
 
 var minMoves = function(A) {
-
+    let output = 0;
+    let checkList = [A[0]];
+    for (let i = 1; i < A.length; i++){
+        let current = A[i];
+        let sorted = false;
+        while (sorted == false){
+            let duplicate = false;
+            for (let num of checkList){
+                if (current == num){
+                    duplicate = true
+                }
+            }
+            if (duplicate == true){
+                current += 1;
+                output += 1;
+            }
+            else{
+                A[i] = current;
+                sorted = true;
+            }
+        }
+    }
+    return output;
 };
